4e6a0355668c8d5a6c2dd210fae642bcd971081c
Global66 <global66@MacBook-Pro-de-Jose.local>
2020-12-11 13:16:26 -0500

    feat: include OMP for paralelization

--- montecarlo.h
+++ montecarlo.h
@@ -3,6 +3,7 @@
 
 #include <random>
 #include <iostream>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -25,6 +26,7 @@ double URDMonteCarloIntegration (double a, double b, int n)
     }
   double sum = 0.0;
 
+#pragma omp parallel for reduction(+:sum)
   for (int i = 1; i <= n; i++)
     {
 
@@ -45,17 +47,21 @@ double RNGMonteCarloIntegration (double a, double b, int n)
     }
   double sum = 0.0;
 
+#pragma omp parallel
+{
   auto s = std::to_string(omp_get_thread_num());
   //std::cout<<omp_get_thread_num()<<std::endl;
   const char* x = s.c_str();
   RngStream gen;
   gen = RngStream_CreateStream(x);
 
+ #pragma omp for reduction(+:sum)
   for (int i = 1; i <= n; i++)
     {
     double r = RngStream_RandU01(gen);
     sum = sum + fun(a+((b-a)*r));
   }
+}
   sum = ((b-a)/n)*sum;
   return sum;
 }
