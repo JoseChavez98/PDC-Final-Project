b62e07615b8dbd0d40a5aa6446f82c8387ea373b
Global66 <global66@MacBook-Pro-de-Jose.local>
2020-12-11 13:10:23 -0500

    feat: Sequential method MonteCarlo Integration using URD & RNGStream

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PDC-Final-Project
+
+Correr con:
+
+` g++ -std=c++17 -o main RngStream.cpp main.cpp `
---
+++ montecarlo.h
@@ -0,0 +1,63 @@
+#ifndef MONTECARLO_H
+#define MONTECARLO_H
+
+#include <random>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "RngStream.h"
+
+double fun(double x) //f(x) = x;
+{
+    return x;
+}
+
+double URDMonteCarloIntegration (double a, double b, int n)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dis(0.0, 1.0);
+
+  if(a > b)
+    {
+    return URDMonteCarloIntegration(b, a, n);
+    }
+  double sum = 0.0;
+
+  for (int i = 1; i <= n; i++)
+    {
+
+    double r = dis(gen);
+    sum = sum + fun(a+((b-a)*r));
+    }
+  sum = ((b-a)/n)*sum;
+  return sum;
+}
+
+double RNGMonteCarloIntegration (double a, double b, int n)
+{
+
+
+  if(a > b)
+    {
+    return RNGMonteCarloIntegration(b, a, n);
+    }
+  double sum = 0.0;
+
+  auto s = std::to_string(omp_get_thread_num());
+  //std::cout<<omp_get_thread_num()<<std::endl;
+  const char* x = s.c_str();
+  RngStream gen;
+  gen = RngStream_CreateStream(x);
+
+  for (int i = 1; i <= n; i++)
+    {
+    double r = RngStream_RandU01(gen);
+    sum = sum + fun(a+((b-a)*r));
+  }
+  sum = ((b-a)/n)*sum;
+  return sum;
+}
+
+#endif
